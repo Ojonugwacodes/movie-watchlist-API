f58b7de30091d72a0ab6d63afe71c26af6fe8d47
Omerah Rhema <omerahrhema@gmail.com>
2026-04-06 14:53:23 +0100

    fix prisma import for node 22

--- src/config/db.js
+++ src/config/db.js
@@ -1,26 +1,29 @@
-import 'dotenv/config'
-import { PrismaClient } from '@prisma/client';
+import 'dotenv/config';
+import pkg from '@prisma/client';
 import { withAccelerate } from '@prisma/extension-accelerate';
+
+const { PrismaClient } = pkg;
 
 const prisma = new PrismaClient({
     accelerateUrl: process.env.DATABASE_URL,
-    log: process.env.NODE_ENV === "development" ? ["query", "error", "warn"] : ["error"]
+    log: process.env.NODE_ENV === "development"
+        ? ["query", "error", "warn"]
+        : ["error"]
 }).$extends(withAccelerate());
 
-const connectdb =  async() => {
+const connectdb = async () => {
     try {
         await prisma.$connect();
         console.log("Database connected successfully");
-    }catch(error) {
-        console.error(`Error connecting to database: ${error.message}`
-        );
-        process.exit(1)
-    };
+    } catch (error) {
+        console.error(`Error connecting to database: ${error.message}`);
+        process.exit(1);
+    }
 };
 
-const disconnectdb = async() => {
+const disconnectdb = async () => {
     await prisma.$disconnect();
-    console.log("Database disconnected successfully")
-}
+    console.log("Database disconnected successfully");
+};
 
 export { connectdb, disconnectdb, prisma };
